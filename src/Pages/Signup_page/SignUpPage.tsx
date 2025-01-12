dfcef509429197f2585dda1b40b091cbbbb285b1
Alqudusy <yusufabdulqudus02@gmail.com>
2025-01-12 01:59:14 +0100

    Designing the sign up page, very tough

--- src/Pages/Signup_page/SignUpPage.tsx
+++ src/Pages/Signup_page/SignUpPage.tsx
@@ -1,9 +1,43 @@
 import React from "react";
+import { FcGoogle } from "react-icons/fc";
+import { HiOutlineMail } from "react-icons/hi";
+import { HiOutlineLockClosed, HiOutlineUser } from "react-icons/hi2";
+import { Link } from "react-router-dom";
+import image from "../../assets/landing_page/signup.svg";
+import "./SignUp.css";
 
 const SignUpPage: React.FC = () => {
     return (
-        <div>
-            <h1>Sign Up Page</h1>
+        <div className="sign-up-page">
+            <div className="img">
+                <img src={image} alt="" />
+            </div>
+            <form className="signup-form">
+                <div className='signup-nav'>
+                    <div>
+                        <Link to='/login'> Login </Link>
+                    </div>
+                    <div className="signup"> Signup </div>
+                </div>
+                <div className="form-grp">
+                    <HiOutlineUser className="signup-icon" />
+                    <input type="text" name="username" id="username" placeholder="Unique username" />
+                </div>
+                <div className="form-grp">
+                    <HiOutlineMail className="signup-icon" />
+                    <input type="email" name="email" id="email" placeholder="Email address" />
+                </div>
+                <div className="form-grp">
+                    <HiOutlineLockClosed className="signup-icon" />
+                    <input type="password" name="password" id="password" placeholder="Strong password" />
+                </div>
+                <button className="signup-btn" type="submit">SignUp</button>
+                <div className="alternative">Or Signup with</div>
+                <button className="signup-google">
+                    <FcGoogle size={25} />
+                    <h2>Google</h2>
+                </button>
+            </form>
         </div>
     );
 }
--- src/components/Landing_page/CallToAction_section/CallToActionSection.tsx
+++ src/components/Landing_page/CallToAction_section/CallToActionSection.tsx
@@ -1,15 +1,21 @@
 import React from "react";
+import call1 from "../../../assets/landing_page/call1.svg";
+import call2 from "../../../assets/landing_page/call2.svg";
 import "./CallToAction.css";
 
 const CallToActionSection: React.FC = () => {
     return (
         <section className="call-to-action-section">
-            <div className="left"></div>
+            <div className="left">
+                <img src={call1} alt="" />
+            </div>
             <div className="middle">
-                <h1>Manage your Cryptos with us now</h1>
+                <h1>Get Started with Secure and Simplified Crypto Management</h1>
                 <button className="action-btn">Try now!</button>
             </div>
-            <div className="right"></div>
+            <div className="right">
+                <img src={call2} alt="" />
+            </div>
         </section>
     );
 }
