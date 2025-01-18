ec20115db248ec40f701e8ca2230fbec2c8152c5
Alqudusy <yusufabdulqudus02@gmail.com>
2025-01-18 12:11:22 +0100

    Working on the portfolio page

--- src/Pages/Portfolio_page/PortfolioPage.tsx
+++ src/Pages/Portfolio_page/PortfolioPage.tsx
@@ -1,9 +1,16 @@
-import React from "react";
+import React, { useState } from "react";
+import NavBar from "../../components/Dashboard/NavBar/NavBar";
+import MobileNav from "../../components/Helpers/Mobile_nav/MobileNav";
+import SideBar from "../../components/Helpers/Sidebar/SideBar";
+import './Portfolio.css';
 
 const PortFolioPage: React.FC = () => {
+    const [toggleNav, setToggleNav] = useState<boolean>(false);
     return (
-        <div>
-            <h1>Portfolio</h1>
+        <div className="portfolio-page">
+            <SideBar toggleNav={toggleNav} currentpage="portfolio" />
+            <MobileNav />
+            <NavBar toggleSideBar={toggleNav} togglenav={setToggleNav} />
         </div>
     );
 }
---
+++ src/components/Portfolio_page/Main_section/MainSection.tsx
@@ -0,0 +1,10 @@
+import './Main.css';
+const MainSection: React.FC = () => {
+    return (
+        <div className="main-section">
+            Main section
+        </div>
+    );
+}
+
+export default MainSection;
