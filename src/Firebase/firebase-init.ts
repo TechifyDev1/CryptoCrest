f78f92b5c72e766b6ea7b6752e95b1b33e41515d
Alqudusy <yusufabdulqudus02@gmail.com>
2025-01-23 18:32:46 +0100

    Worked on firebase but encountered a big error

--- src/Firebase/firebase-init.ts
+++ src/Firebase/firebase-init.ts
@@ -3,6 +3,7 @@ import { getAuth } from 'firebase/auth';
 import { getFirestore } from 'firebase/firestore';
 import { firebaseConfig } from './firebase-config.ts';
 const App = initializeApp(firebaseConfig);
+console.log(firebaseConfig);
 const db = getFirestore(App);
 const auth = getAuth(App);
 export { auth, db };
--- src/Pages/Signup_page/SignUpPage.tsx
+++ src/Pages/Signup_page/SignUpPage.tsx
@@ -1,18 +1,45 @@
-import React from "react";
+import { createUserWithEmailAndPassword, updateProfile } from "firebase/auth";
+import { doc, getDoc, setDoc } from "firebase/firestore";
+import React, { ChangeEvent } from "react";
 import { FcGoogle } from "react-icons/fc";
 import { HiOutlineMail } from "react-icons/hi";
 import { HiOutlineLockClosed, HiOutlineUser } from "react-icons/hi2";
 import { Link } from "react-router-dom";
 import image from "../../assets/landing_page/signup.svg";
+import { auth, db } from "../../Firebase/firebase-init";
 import "./SignUp.css";
 
 const SignUpPage: React.FC = () => {
+    const handleSignUp = async (e: ChangeEvent<HTMLFormElement>) => {
+        e.preventDefault();
+        const formData = new FormData(e.target);
+        const username = formData.get('username') as string;
+        const email = formData.get('email') as string;
+        const password = formData.get('password') as string;
+        try {
+            const userRef = doc(db, 'users', username);
+            const userSnap = await getDoc(userRef);
+            if (userSnap.exists()) throw new Error('Username already exists, please choose another one');
+            const { user } = await createUserWithEmailAndPassword(auth, email, password);
+            const userId = user.uid;
+            const userInfo = {
+                username,
+                email,
+                userId,
+            }
+            await updateProfile(user, { displayName: username });
+            await setDoc(userRef, userInfo);
+            console.log('User created successfully');
+        } catch (e: any) {
+            console.error(e.message);
+        }
+    }
     return (
         <div className="sign-up-page">
             <div className="img">
                 <img src={image} alt="" />
             </div>
-            <form className="signup-form">
+            <form className="signup-form" onSubmit={handleSignUp}>
                 <div className='signup-nav'>
                     <div className="signup"> Signup </div>
                     <div className="signin">
