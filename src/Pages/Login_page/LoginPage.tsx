de1d620e3414a0b36a01098bf59c4bfde5aa27d8
Alqudusy <yusufabdulqudus02@gmail.com>
2025-01-24 19:11:33 +0100

    Worked on logging in with google and also email and password same as the sign up

--- src/Pages/Login_page/LoginPage.tsx
+++ src/Pages/Login_page/LoginPage.tsx
@@ -1,17 +1,60 @@
-import React from "react";
+import { GoogleAuthProvider, signInWithEmailAndPassword, signInWithPopup, signOut } from "firebase/auth";
+import { doc, getDoc } from "firebase/firestore";
+import React, { ChangeEvent, useState } from "react";
 import { FcGoogle } from "react-icons/fc";
 import { HiLockClosed, HiOutlineUser } from "react-icons/hi2";
 import { Link } from "react-router-dom";
 import image from "../../assets/landing_page/login.svg";
+import { auth, db } from "../../Firebase/firebase-init";
 import './Login.css';
 
 const LoginPage: React.FC = () => {
+    const [email, setEmail] = useState<string>('');
+    const handleLogin = async (e: ChangeEvent<HTMLFormElement>) => {
+        e.preventDefault();
+        const formData = new FormData(e.target);
+        const username = formData.get('username') as string;
+        const password = formData.get('password') as string;
+        try {
+            const userRef = doc(db, 'users', username);
+            const usersnap = await getDoc(userRef);
+            if (usersnap.exists()) {
+                const user = usersnap.data();
+                setEmail(user?.email);
+                await signInWithEmailAndPassword(auth, email, password);
+                console.log('User Logged In');
+            } else {
+                throw new Error('Wrong Username or Password');
+            }
+        } catch (e: any) {
+            console.log(e.message);
+        }
+    }
+    const handleGoogleLogin = async () => {
+        const provider = new GoogleAuthProvider();
+        try {
+            const { user } = await signInWithPopup(auth, provider);
+            const username = user?.displayName;
+            if (!username) {
+                await signOut(auth);
+                throw new Error("You're yet to sign up")
+            };
+            const userRef = doc(db, 'users', username);
+            const usersnap = await getDoc(userRef);
+            if (!usersnap.exists()) {
+                await signOut(auth);
+                throw new Error("You're yet to sign up")
+            }
+        } catch (e: any) {
+            console.log(e.message);
+        }
+    }
     return (
         <div className="login-page">
             <div className="img">
                 <img src={image} alt="login image" />
             </div>
-            <form className="login-form">
+            <form className="login-form" onSubmit={handleLogin}>
                 <div className="login-nav">
                     <div className="signup">
                         <Link to='/signup' style={{ textDecoration: 'none', color: 'var(--text-color)' }}> SignUp </Link>
@@ -31,7 +74,7 @@ const LoginPage: React.FC = () => {
                 </div>
                 <button className="login-btn" type="submit">Login</button>
                 <div className="alternative">Or Login with</div>
-                <button className="login-google">
+                <button className="login-google" type="button" onClick={handleGoogleLogin}>
                     <FcGoogle size={25} className="google-icon" />
                     <h2>oogle</h2>
                 </button>
