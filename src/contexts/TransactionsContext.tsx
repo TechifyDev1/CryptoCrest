60a2b3aaab42291ec2ed7c81c92187a722a151d4
TechifyDev <yusufabdulqudus02@gmail.com>
2025-01-28 02:09:40 -0800

    Working on the realtime syncing of firestore

--- src/contexts/TransactionsContext.tsx
+++ src/contexts/TransactionsContext.tsx
@@ -1,7 +1,6 @@
-import { doc, getDoc } from 'firebase/firestore';
+import { doc, onSnapshot } from 'firebase/firestore';
 import React, { createContext, useEffect, useState } from 'react';
 import { auth, db } from '../Firebase/firebase-init';
-import { toast } from 'sonner';
 import { Transaction } from '../type';
 
 interface TransactionContextType {
@@ -17,29 +16,19 @@ const TransactionsContextProvider: React.FC<{ children: React.ReactNode }> = ({
 }) => {
   const [transactions, setTransactions] = useState<any[]>([]);
   useEffect(() => {
-    const fetchTransactions = async () => {
-      try {
-        const username = auth.currentUser?.displayName;
-        if (!username) {
-          throw new Error('Please login to view transactions');
-        }
-        const userRef = doc(db, 'users', username.toLowerCase());
-        const transactionsSnap = await getDoc(userRef);
-        if (transactionsSnap.exists()) {
-          const transactionsData = transactionsSnap.data();
-          console.log(transactionsData);
-          setTransactions(transactionsData.transactions);
-        } else {
-          throw new Error('No transactions found');
-        }
-      } catch (error: any) {
-        toast.error(error.message);
-      }
-    };
+    let unsubscribeTransactions: () => void = () => {};
 
     const unsubscribe = auth.onAuthStateChanged((user) => {
-      if (user) {
-        fetchTransactions();
+      if (user && user.displayName) {
+        unsubscribeTransactions = onSnapshot(
+          doc(db, 'users', user.displayName.toLowerCase()),
+          (doc) => {
+            const data = doc.data();
+            if (data) {
+              setTransactions(data.transactions);
+            }
+          }
+        );
       }
     });
 
