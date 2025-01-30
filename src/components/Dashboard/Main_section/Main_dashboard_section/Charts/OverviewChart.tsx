fcf6b1fabbd67e83c20066a11317bcb0415dd643
TechifyDev <yusufabdulqudus02@gmail.com>
2025-01-30 22:24:33 +0100

    Worked on the market price updated some styles too

--- src/components/Dashboard/Main_section/Main_dashboard_section/Charts/OverviewChart.tsx
+++ src/components/Dashboard/Main_section/Main_dashboard_section/Charts/OverviewChart.tsx
@@ -1,25 +1,41 @@
-import ReactECharts from "echarts-for-react";
-import React, { useEffect, useRef, useState } from "react";
+import ReactECharts from 'echarts-for-react';
+import React, { useEffect, useRef, useState } from 'react';
 
 const OverviewChart: React.FC<{ option: {} }> = ({ option }) => {
-    const chartContainer = useRef<HTMLDivElement>(null);
-    const [isReady, setIsReady] = useState<boolean>(false);
-    useEffect(() => {
-        const checkReadiness = (): void => {
-            if (chartContainer.current) {
-                const { clientWidth, clientHeight } = chartContainer.current;
-                if (clientHeight > 0 && clientWidth > 0) setIsReady(true);
-            } else {
-                setTimeout(checkReadiness, 100);
-            }
-        }
-        checkReadiness();
-    }, []);
-    return (
-        <div style={{ height: "50%", width: "auto", display: 'flex', justifyContent: 'center', alignItems: 'center' }} ref={chartContainer}>
-            {isReady ? (<ReactECharts option={option} style={{ height: 100, width: "100%" }} />) : (<div>--:--</div>)}
-        </div>
-    )
-}
+  const chartContainer = useRef<HTMLDivElement>(null);
+  const [isReady, setIsReady] = useState<boolean>(false);
+  useEffect(() => {
+    const checkReadiness = (): void => {
+      if (chartContainer.current) {
+        const { clientWidth, clientHeight } = chartContainer.current;
+        if (clientHeight > 0 && clientWidth > 0) setIsReady(true);
+      } else {
+        setTimeout(checkReadiness, 100);
+      }
+    };
+    checkReadiness();
+  }, []);
+  return (
+    <div
+      style={{
+        height: '300px',
+        width: '100%',
+        display: 'flex',
+        justifyContent: 'center',
+        alignItems: 'center',
+      }}
+      ref={chartContainer}
+    >
+      {isReady ? (
+        <ReactECharts
+          option={option}
+          style={{ height: '100%', width: '100%' }}
+        />
+      ) : (
+        <div>--:--</div>
+      )}
+    </div>
+  );
+};
 
 export default OverviewChart;
--- src/components/Dashboard/Main_section/Main_dashboard_section/OverviewContainer.tsx
+++ src/components/Dashboard/Main_section/Main_dashboard_section/OverviewContainer.tsx
@@ -4,42 +4,54 @@ import './Overview.css';
 import { doc, getDoc } from 'firebase/firestore';
 import { auth, db } from '../../../../Firebase/firebase-init';
 import { toast } from 'sonner';
+import axios from 'axios';
 
 const OverviewContainer: React.FC = () => {
-  const [purchaseData, setPurchaseData] = useState<any[]>([]);
-  const [purchaseDates, setPurchaseDates] = useState<any[]>([]);
+  const [dates, setDates] = useState<any[]>([]);
   const [purchaseAmounts, setPurchaseAmount] = useState<any[]>([]);
+  const [revenueAmounts, setRevenueAmounts] = useState<any[]>([]);
+  const [marketPrices, setMarketPrices] = useState<any[]>([]);
   const fetchedRef = useRef<boolean>(false);
+
   useEffect(() => {
     const getPurchaseData = async () => {
       try {
-        if (fetchedRef.current === true) return;
+        if (fetchedRef.current) return;
         const username = auth.currentUser?.displayName?.toLowerCase();
         if (!username) throw new Error('Please sign in to view data');
+
         const userRef = doc(db, 'users', username);
         const data = (await getDoc(userRef)).data();
         if (!data) throw new Error('No data found');
+
         const transactions = data.transactions;
         const purchases = transactions.filter(
-          (transaction: any) => transaction.type.toLowerCase() === 'buy'
+          (t: any) => t.type.toLowerCase() === 'buy'
         );
-        const formattedDates: any[] = [];
-        const unformattedDates = purchases.map((obj: any) => obj.date);
-        setPurchaseAmount(purchases.map((obj: any) => obj.amount));
-        unformattedDates.forEach((date: any) => {
-          let dateObj = new Date(date);
-          const formattedDate = dateObj.toLocaleDateString('en-GB', {
-            day: 'numeric',
-            month: 'short',
-            year: 'numeric',
-          });
-          formattedDates.push(formattedDate);
-        });
-        setPurchaseDates(formattedDates);
-        setPurchaseData(purchases);
-        console.log(purchases);
-        console.log(purchases.map((obj: any) => obj.date));
-        console.log(formattedDates);
+        const revenue = transactions.filter(
+          (t: any) => t.type.toLowerCase() === 'sell'
+        );
+
+        const marketPrice = await axios.get(
+          'https://api.coingecko.com/api/v3/coins/ethereum/market_chart?vs_currency=usd&days=30'
+        );
+
+        setMarketPrices(marketPrice.data.prices.map((entry: any) => entry[1]));
+
+        setDates(
+          marketPrice.data.prices.map((entry: any) => {
+            return new Date(entry[0]).toLocaleDateString('en-GB', {
+              day: 'numeric',
+              month: 'short',
+            });
+          })
+        );
+
+        console.log((await marketPrice).data);
+
+        setPurchaseAmount(purchases.map((t: any) => t.amount));
+        setRevenueAmounts(revenue.map((t: any) => t.amount));
+
         toast.success('Data fetched successfully');
         fetchedRef.current = true;
       } catch (error: any) {
@@ -47,118 +59,63 @@ const OverviewContainer: React.FC = () => {
         toast.error(error.message);
       }
     };
+
     const unsub = auth.onAuthStateChanged((user) => {
-      if (user) {
-        getPurchaseData();
-      } else {
-        toast.error('Please sign in to continue');
-        console.log('no user');
-      }
+      if (user) getPurchaseData();
+      else toast.error('Please sign in to continue');
     });
+
     return () => unsub();
   }, []);
+
   const options = {
     title: {
       text: 'Crypto Price Trend',
       left: 'center',
-      show: false,
+      show: true,
     },
     tooltip: {
       trigger: 'axis',
     },
     xAxis: {
       type: 'category',
-      data: ['Jan', 'Feb', 'Mar', 'Apr', 'May', 'Jun'],
-      show: false,
+      data: dates,
+      show: true,
     },
     yAxis: {
-      show: false,
+      show: true,
       type: 'value',
     },
     series: [
       {
-        name: 'ETH',
-        type: 'line',
-        data: [4001, 1222, 3432, 2345, 2334, 4567],
-        smooth: false,
-      },
-    ],
-  };
-  const purchaseOptions = {
-    title: {
-      text: 'Crypto Price Trend',
-      left: 'center',
-      show: false,
-    },
-    tooltip: {
-      trigger: 'axis',
-    },
-    xAxis: {
-      type: 'category',
-      data: purchaseDates,
-      show: false,
-    },
-    yAxis: {
-      show: false,
-      type: 'value',
-    },
-    series: [
-      {
+        name: 'Purchases',
         type: 'line',
         data: purchaseAmounts,
         smooth: false,
       },
+      {
+        name: 'Revenue',
+        type: 'line',
+        data: revenueAmounts,
+        smooth: false,
+      },
+      {
+        name: 'Market trends',
+        type: 'line',
+        data: marketPrices,
+        smooth: false,
+      },
     ],
   };
+
   return (
     <div className="overview-container">
       <div className="header">
         <h2>Dashboard</h2>
-        <span>Dashboard/crypto current</span>
+        <span>Dashboard / Crypto Overview</span>
       </div>
       <div className="charts-container">
-        <div
-          className="chart"
-          style={{
-            display: 'flex',
-            justifyContent: 'center',
-            alignItems: 'center',
-          }}
-        >
-          <div>
-            <span className="title">purchase</span>
-            <OverviewChart option={purchaseOptions} />
-          </div>
-          <span className="amount">$14,400</span>
-        </div>
-        <div
-          className="chart"
-          style={{
-            display: 'flex',
-            justifyContent: 'center',
-            alignItems: 'center',
-          }}
-        >
-          <div>
-            <span className="title">revenue</span>
-            <OverviewChart option={options} />
-          </div>
-          <span className="amount">$14,400</span>
-        </div>
-        <div
-          className="chart"
-          style={{
-            display: 'flex',
-            justifyContent: 'center',
-            alignItems: 'center',
-          }}
-        >
-          <div>
-            <span className="title">market price</span>
-            <OverviewChart option={options} />
-          </div>
-          <span className="amount">$14,400</span>
-        </div>
+        <OverviewChart option={options} />
       </div>
     </div>
   );
