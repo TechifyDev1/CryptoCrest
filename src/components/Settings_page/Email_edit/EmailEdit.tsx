b1e16b57eb91383a58e91987d3068e1b4c9ac1c6
TechifyDev <yusufabdulqudus02@gmail.com>
2025-02-18 22:48:17 +0100

    User can now update his email

--- src/components/Settings_page/Email_edit/EmailEdit.tsx
+++ src/components/Settings_page/Email_edit/EmailEdit.tsx
@@ -1,10 +1,26 @@
+import { updateEmail } from "firebase/auth";
 import "./email-edit.css";
+import { auth } from "../../../Firebase/firebase-init";
+import { ChangeEvent } from "react";
+import { toast } from "sonner";
 const EmailEdit: React.FC<{toggleEmailEdit: boolean}> = ({toggleEmailEdit}) => {
+    const handleChangeEmail = async (e: ChangeEvent<HTMLFormElement>): Promise<void> => {
+        e.preventDefault();
+        try {
+            const user = auth.currentUser;
+            if (user?.email !== e.target.oldEmail) throw new Error("Invalid previous email")
+            if (!user) throw new Error("You have no account here");
+            updateEmail(user, e.target.newEmail);
+            toast.success("Email updated successfully");
+        } catch(e: any) {
+            toast.error(e.message);
+        }
+    }
     return(
         <div className="email-edit-container" style={{transform: toggleEmailEdit ? "" : "translateY(100%)"}}>
-            <form className="email-edit-form">
-                <input type="email" name="old-email" id="old-email" placeholder="Enter your old email" />
-                <input type="email" name="new-email" id="new-email" placeholder="Enter your new email" />
+            <form className="email-edit-form" onSubmit={handleChangeEmail}>
+                <input type="email" name="oldEmail" id="old-email" placeholder="Enter your old email" required/>
+                <input type="email" name="newEmail" id="new-email" placeholder="Enter your new email" required/>
                 <button type="submit" className="update-btn">Update!</button>
             </form>
         </div>
